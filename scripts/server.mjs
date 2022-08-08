9f1c69a86f1ab9e33212593d39b91bbf792e8416
tyhopp <hopp.ty.c@gmail.com>
2022-08-08 12:20:49 +0800

    Reproduce issue

--- scripts/server.mjs
+++ scripts/server.mjs
@@ -7,17 +7,40 @@ const port = 8888;
 const server = http.createServer((req, res) => listen(req, res));
 
 async function listen(req, res) {
-  const { filePath, status } = resolveFile(req.url);
+  const { filePath, statusCode, contentType } = resolveFile(req.url);
   const file = await readFile(filePath);
-  res.setHeader("Content-Type", "text/html");
-  res.writeHead(status);
+  res.setHeader("Content-Type", contentType);
+  res.writeHead(statusCode);
   res.end(file);
 }
 
 function resolveFile(url) {
-  return url === "/"
-    ? { filePath: "public/index.html", status: 200 }
-    : { filePath: "public/404.html", status: 404 };
+  switch (url) {
+    case "/":
+      return {
+        filePath: "public/index.html",
+        statusCode: 200,
+        contentType: "text/html",
+      };
+    case "/assets/font/fira-mono-400.woff2":
+      return {
+        filePath: `public${url}`,
+        statusCode: 200,
+        contentType: "font/woff2",
+      };
+    case "/assets/font/fira-mono-700.woff2":
+      return {
+        filePath: `public${url}`,
+        statusCode: 200,
+        contentType: "font/woff2",
+      };
+    default:
+      return {
+        filePath: "public/404.html",
+        statusCode: 404,
+        contentType: "text/html",
+      };
+  }
 }
 
 server.listen(port, hostname, () => {
